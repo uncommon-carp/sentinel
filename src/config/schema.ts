6514c34d5948e0c6713dd185eea6f3b5d1524ed0
Cory Sorel <116194787+uncommon-carp@users.noreply.github.com>
2026-05-11 16:14:13 -0500

    chore: refactor suite aggregation (#34)

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -59,8 +59,7 @@ export const SentinelConfigSchema = z.object({
       cors: z.boolean().default(true),
       auth: z.boolean().default(true),
       ratelimit: z.boolean().default(true),
-      inventory: z.boolean().default(true),
-      injection: z.boolean().default(false)
+      inventory: z.boolean().default(true)
     })
     .default({}),
   scope: ScopeSchema.default({}),
